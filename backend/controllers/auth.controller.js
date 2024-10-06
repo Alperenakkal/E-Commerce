9fde365029aa805011080cf5fd3b0b8cb4468d65
Alperenakkal <alperenakkal06@gmail.com>
2024-10-06 23:02:25 +0300

    connecttomongodb

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const signup = async (req,res)=>{
+    
+    res.send("Sign up route calles ")
+   
+}
+export const login = async (req,res)=>{
+    
+    res.send("Sign up route calles ")
+   
+}
+export const logout = async (req,res)=>{
+    
+    res.send("Sign up route calles ")
+   
+}
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -9,6 +11,12 @@ const app= express();
 const PORT = process.env.PORT || 5000;
 
 
+
+app.use("/api/auth",authRoutes)
+
+
 app.listen(PORT,()=>{
     console.log("Server is running on http://localhost:"+PORT);
+
+    connectDB();
 });
