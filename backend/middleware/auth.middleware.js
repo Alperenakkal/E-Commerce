3f34a3defa982a63a37f0f18430c367d8347e96e
Alperenakkal <alperenakkal06@gmail.com>
2024-10-12 22:38:45 +0300

    coupon and cart

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -12,13 +12,15 @@ export const protectRoute = async (req, res, next) => {
         }
 
         try {
-            const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+            const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
+
+           
         const user= await User.findById(decoded.userId).select("-password");
         if (!user) {
             return res.status(401).json({ message: "user not found" });
         } 
 
-        req.user = user;
+     req.user = user;
         next();
         } catch (error) {
             if (error.name === "TokenExpiredError") {
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import coupunRoutes from "./routes/coupon.route.js";
+
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -17,6 +20,9 @@ app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/products",productRoutes)
+app.use("/api/cart",cartRoutes)
+app.use("/api/coupons",coupunRoutes)
+
 
 
 app.listen(PORT,()=>{
@@ -24,3 +30,4 @@ app.listen(PORT,()=>{
 
     connectDB();
 });
+
